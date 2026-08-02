7767c5950a62ac697bd0349c55fd177b0482223a
Sagar Mal <sagarnehra47@gmail.com>
2026-08-02 15:54:55 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
-variables "aws_subnet" {
+variable "aws_subnet" {
     cidr_block_subnet = "10.0.0.0/28"
     subnet_tag_name = "aws_public_subnet"
 }
 
-variables "aws_vpc" {
+variable "aws_vpc" {
      cidr_vpc_block = "10.0.0.0/28"
        cidr_tag_name = "aws_cidr_test"
 
